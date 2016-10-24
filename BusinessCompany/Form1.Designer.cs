092a6fa8e6e9292480eeb0625b0817cb032a05d9
Sandrine Delecourt <sandredlcrt@hotmail.fr>
2016-10-24 14:45:06 +0200

    timer 1 second ok

--- BusinessCompany/Form1.Designer.cs
+++ BusinessCompany/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CA = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CA
+            // 
+            this.CA.Location = new System.Drawing.Point(13, 13);
+            this.CA.Name = "CA";
+            this.CA.Size = new System.Drawing.Size(100, 20);
+            this.CA.TabIndex = 0;
+            this.CA.Text = "3000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 443);
+            this.Controls.Add(this.CA);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox CA;
     }
 }
 
--- BusinessCompany/Form1.cs
+++ BusinessCompany/Form1.cs
@@ -12,9 +12,21 @@ namespace BusinessCompany
 {
     public partial class Form1 : Form
     {
+        static Timer timer1 = new Timer();
         public Form1()
         {
             InitializeComponent();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; //(1 second)
+            timer1.Start();
+        }
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            int chiffre = int.Parse(CA.Text);
+
+            CA.Text = ((int)chiffre - 1).ToString();
+
         }
     }
 }
